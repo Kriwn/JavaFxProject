6bee1d091859e29528bdbb778c495f1b7e4374ae
ThanawatPtd <ThanawatPotidech@gmail.com>
2023-08-23 13:52:53 +0700

    create NormalUserDatasource

--- src/main/java/cs211/project/models/Account.java
+++ src/main/java/cs211/project/models/Account.java
@@ -22,4 +22,24 @@ public class Account {
     public void setImage(String image){
         this.image = image;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImage() {
+        return image;
+    }
 }
--- src/main/java/cs211/project/models/NormalUserList.java
+++ src/main/java/cs211/project/models/NormalUserList.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 
 public class NormalUserList {
     private ArrayList<NormalUser> users;
-
+    public NormalUserList(){
+        users = new ArrayList<>();
+    }
     public void addNewUser(String username, String name, String password){
+        username = username.trim();
+        name = name.trim();
+        password = password.trim();
+        if(!username.equals("") && !name.equals("") && password.equals("")){
 
+        }
     }
     public NormalUser findUserByUsername(String username){
        return null;
