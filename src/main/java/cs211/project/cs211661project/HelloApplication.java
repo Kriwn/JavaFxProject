6e0db86226103b99bdcb753dbb061a70f97b6072
ThanawatPtd <ThanawatPotidech@gmail.com>
2023-08-29 13:04:42 +0700

    modified helloapp

--- src/main/java/cs211/project/cs211661project/HelloApplication.java
+++ src/main/java/cs211/project/cs211661project/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         NPBPRouter.setCss("CSS/User/theme-1.css");
         configRoute();
 //        String css = getClass().getResource("css/theme-2.css").toExternalForm();
-        NPBPRouter.goTo("create-event");
+        NPBPRouter.goTo("app");
     }
 
     public void configRoute(){
