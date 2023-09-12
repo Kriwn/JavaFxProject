1bf76917f96193bacd4de92fbbf05910ff75f53e
PALM <thanapoom.cha@ku.th>
2023-09-12 17:02:48 +0700

    Edit Team and Staff

---
+++ src/main/java/cs211/project/controllers/TeamDetailController.java
@@ -0,0 +1,17 @@
+package cs211.project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class TeamDetailController {
+    @FXML private AnchorPane page;
+    @FXML private Label eventNameLabel;
+    @FXML private Label teamNameLabel;
+    @FXML private Label dateStartEventLabel;
+    @FXML private Label timeStartEventLabel;
+    @FXML private Label dateEndEventLabel;
+    @FXML private Label timeEndEventLabel;
+    @FXML private Label countMemberLabel;
+    @FXML private Label maxMemberLabel;
+}
