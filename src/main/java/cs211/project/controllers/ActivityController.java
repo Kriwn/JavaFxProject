019534e1aa7ce035154773bf0bf78dcb393cc561
Kriwn <nonisgod@gmail.com>
2023-09-19 15:14:06 +0700

    before pull

--- src/main/java/cs211/project/controllers/ActivityController.java
+++ src/main/java/cs211/project/controllers/ActivityController.java
@@ -2,11 +2,8 @@ package cs211.project.controllers;
 
 import cs211.project.models.Activity;
 import cs211.project.models.ActivityList;
-<<<<<<< HEAD
 import cs211.project.services.EventActivityDatasource;
-=======
 //import cs211.project.services.ActivityDatasource;
->>>>>>> 06300bf1e6e07fc8174ef53734cb50956c52c60b
 import cs211.project.services.Datasource;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
